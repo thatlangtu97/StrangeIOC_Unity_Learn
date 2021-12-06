5e497cd2e7b81f5def5d4285be08b2b8dacb0db0
hieunq2 <hieunq2@bravestars.com>
2021-12-07 00:14:16 +0700

    add Animation Logo

--- StrangeIOC_Learn/Assets/Game/Scripts/FlashScript/View/PlayFlashScene.cs
+++ StrangeIOC_Learn/Assets/Game/Scripts/FlashScript/View/PlayFlashScene.cs
@@ -1,25 +1,28 @@
-﻿using EntrySystem;
-using strange.extensions.context.impl;
+﻿using System.Collections;
 using strange.extensions.mediation.impl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayFlashScene : View
 {
     public string nameScene;
     public float timeDelay;
-    float currentTime = 0;
+    public Button StartGameBtn;
+    public Animator animator;
     protected override void Start()
     {
         base.Start();
     }
-    void Update()
+    public void StartGameClick()
     {
-        currentTime += Time.deltaTime;
-        if(currentTime>= timeDelay)
-        {
-            SceneManager.LoadScene(nameScene);
-            Destroy(gameObject);
-        }
+        
+        StartCoroutine(DelayLoadScene());
+    }
+    IEnumerator DelayLoadScene()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetTrigger("HideLogo");
+        SceneManager.LoadScene(nameScene);
     }
 }
