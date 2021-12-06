a91becea243dafd41ab38aa80570171e6efb2313
hieunq2 <hieunq2@bravestars.com>
2021-12-07 00:57:58 +0700

    add animLogo

--- StrangeIOC_Learn/Assets/Game/Scripts/FlashScript/View/PlayFlashScene.cs
+++ StrangeIOC_Learn/Assets/Game/Scripts/FlashScript/View/PlayFlashScene.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class PlayFlashScene : View
 {
     public string nameScene;
-    public float timeDelay;
     public Button StartGameBtn;
     public Animator animator;
     protected override void Start()
@@ -21,8 +20,8 @@ public class PlayFlashScene : View
     }
     IEnumerator DelayLoadScene()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.02f);
         animator.SetTrigger("HideLogo");
-        SceneManager.LoadScene(nameScene);
+        SceneManager.LoadSceneAsync(nameScene);
     }
 }
