5993b6370503917e9253165834d29d1691a796c5
ToanCute <toanlt@bravestars.com>
2021-12-14 20:11:20 +0700

    push skill state

---
+++ StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/ScriptsBT/SkillConfig/SkillComboConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SkillComboConfig", menuName = "CoreGame/SkillComboConfig")]
+public class SkillComboConfig : ScriptableObject
+{
+    public List<SkillConfig> skillDatas;
+}
+[System.Serializable]
+public class SkillConfig
+{
+    public int id;
+    public AnimationCurve curveX,curveY;
+    public string nameTrigger;
+    public float durationAnimation;
+    public string nameEvent;
+    public bool lockVelocity;
+    //public Vector2 velocity;
+    //public float durationVelocity;
+}
