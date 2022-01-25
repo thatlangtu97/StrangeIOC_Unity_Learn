c1bfb56a3e754012ae9f293aa980f5cbd79e5823
ToanCute <toanlt@bravestars.com>
2022-01-25 19:09:27 +0700

    fix BT e2

--- StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/ScriptsBT/BT/Action/New Folder/ChangeSkillState.cs
+++ StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/ScriptsBT/BT/Action/New Folder/ChangeSkillState.cs
@@ -7,25 +7,26 @@ namespace CoreBT
     {
         public SharedComponentManager componentManager;
         public int skillId;
-        public float baseTimeCooldown;
-        public float timeCountCooldown;
+        //public float baseTimeCooldown;
+        //public float timeCountCooldown;
         public override void OnStart()
         {
+            componentManager.Value.stateMachine.OnInputSkill(skillId);
             base.OnStart();
         }
-        public override TaskStatus OnUpdate()
-        {
-            if (timeCountCooldown < 0f)
-            {
-                componentManager.Value.stateMachine.OnInputSkill(skillId);
-                timeCountCooldown = baseTimeCooldown;
-                return TaskStatus.Success;
-            }
-            else
-            {
-                timeCountCooldown -= Time.deltaTime;
-                return TaskStatus.Failure;
-            }
-        }
+        //public override TaskStatus OnUpdate()
+        //{
+        //    if (timeCountCooldown < 0f)
+        //    {
+        //        componentManager.Value.stateMachine.OnInputSkill(skillId);
+        //        timeCountCooldown = baseTimeCooldown;
+        //        return TaskStatus.Success;
+        //    }
+        //    else
+        //    {
+        //        timeCountCooldown -= Time.deltaTime;
+        //        return TaskStatus.Failure;
+        //    }
+        //}
     }
 }
