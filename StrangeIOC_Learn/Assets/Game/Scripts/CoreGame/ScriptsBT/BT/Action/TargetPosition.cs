a97ae7af67ba883e66ab3c90f66eea7f0f0d5f66
ToanCute <toanlt@bravestars.com>
2022-01-26 19:04:01 +0700

    skill1 , skill2 E10 , attack 2 E10

---
+++ StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/ScriptsBT/BT/Action/TargetPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+namespace CoreBT
+{
+    [TaskCategory("Extension")]
+    public class TargetPosition : Action
+    {
+        public SharedComponentManager componentManager;
+        //public SharedVector3 targetPosition;
+        public override void OnStart()
+        {
+            base.OnStart();
+            componentManager.Value.stateMachine.transform.position = componentManager.Value.stateMachine.componentManager.enemy.position;
+        }
+    }
+}
