953a28f14406763cc544f0c4a903c101375f5b5d
ToanCute <toanlt@bravestars.com>
2022-02-06 02:16:42 +0700

    fix projectile arrow

--- StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/GamePlay/Collider/ProjectileCollider.cs
+++ StrangeIOC_Learn/Assets/Game/Scripts/CoreGame/GamePlay/Collider/ProjectileCollider.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
 
 public class ProjectileCollider : MonoBehaviour
 {
     public ProjectileComponent component;
+    [FoldoutGroup("CAST BOX COLLIDER")]
+    //[BoxGroup("Cast Collider")]
+    public PowerCollider powerCollider;
+
+    [FoldoutGroup("CAST BOX COLLIDER")]
+    //[BoxGroup("Cast Collider")]
+    public Vector2 force;
     public virtual void OnTriggerEnter2D(Collider2D other)
     {
-        DealDmgManager.DealDamage(other, component.entity, PowerCollider.Node);
+        //DealDmgManager.DealDamage(other, component.entity, PowerCollider.Node);
         //component.colliderProjectile.enabled = false;
+        
+        Action action = delegate
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, other.GetComponent<Rigidbody2D>().velocity.y);
+            other.GetComponent<Rigidbody2D>().AddForceAtPosition(new Vector2(force.x * transform.localScale.x, force.y), other.transform.position);
+        };
+        DealDmgManager.DealDamage(other, component.entity, powerCollider, action);
     }
 }
